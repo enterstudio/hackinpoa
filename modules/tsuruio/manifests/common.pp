30bc1d049c7fe815f1a2d86284881aaf580ce767
Paulo Sousa <paulo.sousa@corp.globo.com>
2015-03-20 09:52:41 -0300

    common: set Sao_Paulo timezone

--- modules/tsuruio/manifests/common.pp
+++ modules/tsuruio/manifests/common.pp
@@ -6,4 +6,10 @@ class tsuruio::common {
     hour    => '*'
   } 
 
+  exec { 'set Sao_Paulo timezone' :
+    command => 'echo "America/Sao_Paulo" | tee /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata',
+    path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+    unless  => 'cat /etc/timezone | grep Sao_Paulo'
+  }
+
 }
